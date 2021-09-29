1444648b0dcc97974481befefcb37ec73095d4c6
stuart-payne <paynestu@gmail.com>
2021-09-29 17:30:09 +0100

    Visual Overhaul + range indicator fixes

--- Assets/Scripts/RangeIndicator.cs
+++ Assets/Scripts/RangeIndicator.cs
@@ -7,7 +7,8 @@ namespace ProtoTD
         public void UpdateRadius(float radius)
         {
             // radius needs to be doubled for scale to be accurate
-            transform.localScale = new Vector3(1, 1, 1) * (radius * 2); 
+            var lossyScale = transform.lossyScale;
+            transform.localScale = new Vector3((1 /lossyScale.x), 1 /lossyScale.y, 1/lossyScale.z) * (radius * 2); 
         }
     }
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace ProtoTD
@@ -7,23 +8,32 @@ namespace ProtoTD
         [SerializeField] private GameObject projectilePrefab;
         [SerializeField] private GameObject projectileSpawnPoint;
         [SerializeField] private AudioSource m_ProjectileSound;
+        [SerializeField] private LineRenderer m_LineRenderer;
 
         protected override void Update()
         {
-            if(m_TargetSelector.CurrentTarget != null)
-                transform.LookAt(m_TargetSelector.CurrentTarget.transform);
+            // if(m_TargetSelector.CurrentTarget != null)
+            //     transform.LookAt(m_TargetSelector.CurrentTarget.transform);
             base.Update();
         }
 
         protected override void Fire()
         {
-            GameObject proj = Instantiate(projectilePrefab, projectileSpawnPoint.transform.position, projectilePrefab.transform.rotation);
-            var projComp = proj.GetComponent<Projectile>();
-            projComp.Target = m_TargetSelector.CurrentTarget;
-            projComp.Damage = Stats.Damage;
+            var enemyComp = m_TargetSelector.CurrentTarget.GetComponent<Enemy>();
+            enemyComp.DealDamage(Stats.Damage);
             if (Stats.AppliesStatusEffect)
-                projComp.StatusEffect = Stats.StatusEffect;
+                enemyComp.Stats.StatusEffects.Add(Stats.StatusEffect);
             m_ProjectileSound.Play();
+            StartCoroutine(DisplayBeamEffect(new[] {transform.position, m_TargetSelector.CurrentTarget.transform.position}));
+        }
+        
+        IEnumerator DisplayBeamEffect(Vector3[] linePositions)
+        {
+            m_LineRenderer.gameObject.SetActive(true);
+            m_LineRenderer.positionCount = linePositions.Length;
+            m_LineRenderer.SetPositions(linePositions);
+            yield return new WaitForSeconds(0.1f);
+            m_LineRenderer.gameObject.SetActive(false);
         }
     }
 }
